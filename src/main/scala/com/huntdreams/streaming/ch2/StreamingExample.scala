be5159459936795a116f26296bd1733e595ab944
noprom <tyee.noprom@qq.com>
2016-02-21 14:01:47 +0800

    Streaming scala version implemennted

--- src/main/scala/com/huntdreams/streaming/ch2/StreamingExample.scala
+++ src/main/scala/com/huntdreams/streaming/ch2/StreamingExample.scala
@@ -1,6 +1,8 @@
 package com.huntdreams.streaming.ch2
 
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
@@ -22,5 +24,35 @@ object StreamingExample {
     //It will further read the Data and convert it into DStream
 
     val lines = streamCtx.socketStream("localhost", 9087)
+    //Apply the Split() function to all elements of DStream
+    //which will further generate multiple new records from each record in Source Stream
+    //And then use flatmap to consolidate all records and create a new DStream.
+    val words = lines.flatMap(x => x.split(" "))
+    //Now, we will count these words by applying a using map()
+    //map() helps in applying a given function to each element in an RDD.
+    val pairs = words.map(word => (word, 1))
+    //Further we will aggregate the value of each key by using/ applying the given function.
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    //Lastly we will print all Values
+    //wordCounts.print(20)
+
+    printValues(wordCounts,streamCtx)
+    //Most important statement which will initiate the Streaming Context
+    streamCtx.start();
+    //Wait till the execution is completed.
+    streamCtx.awaitTermination();
+  }
+
+  def printValues(stream: DStream[(String, Int)], streamCtx: StreamingContext): Unit = {
+    stream.foreachRDD(foreachFunc)
+    def foreachFunc = (rdd: RDD[(String, Int)]) => {
+      val array = rdd.collect()
+      println("---------Start Printing Results----------")
+      for (res <- array) {
+        println(res)
+      }
+      println("---------Finished Printing Results----------")
+    }
   }
 }
