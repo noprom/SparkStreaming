b715aebb8ba61bf2195fb2900714df6917637b46
noprom <tyee.noprom@qq.com>
2016-02-22 17:24:18 +0800

    add flume pom

--- src/main/scala/com/huntdreams/streaming/ch3/ScalaLoadDistributedEvents.scala
+++ src/main/scala/com/huntdreams/streaming/ch3/ScalaLoadDistributedEvents.scala
@@ -19,10 +19,15 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 object ScalaLoadDistributedEvents {
 
   def main(args: Array[String]) {
-    println("Creating spark configuration")
+    println("Creating Spark Configuration")
+    //Create an Object of Spark Configuration
     val conf = new SparkConf()
-    conf.setAppName("Streaming data loading application")
+    //Set the logical and user defined Name of this Application
+    conf.setAppName("Streaming Data Loading Application")
+
     println("Retreiving Streaming Context from Spark Conf")
+    //Retrieving Streaming Context from SparkConf Object.
+    //Second parameter is the time interval at which streaming data will be divided into batches
     val streamCtx = new StreamingContext(conf, Seconds(2))
 
     //Create an Array of InetSocketaddress containing the  Host and the Port of the machines
@@ -32,9 +37,10 @@ object ScalaLoadDistributedEvents {
     //2. a1.sinks.spark.port
     //3. a2.sinks.spark1.hostname
     //4. a2.sinks.spark1.port
-    var address = new Array[InetSocketAddress](2)
-    address(0) = new InetSocketAddress("localhost", 4949)
-    address(1) = new InetSocketAddress("localhost", 4950)
+    var addresses = new Array[InetSocketAddress](2);
+
+    addresses(0) = new InetSocketAddress("localhost",4949)
+    addresses(1) = new InetSocketAddress("localhost",4950)
 
     //Create a Flume Polling Stream which will poll the Sink the get the events
     //from sinks every 2 seconds.
@@ -44,30 +50,32 @@ object ScalaLoadDistributedEvents {
     //2.parallelism  - The Number of concurrent requests this stream should send to the sink.
     //for more information refer to
     //https://spark.apache.org/docs/1.1.0/api/java/org/apache/spark/streaming/flume/FlumeUtils.html
+    val flumeStream = FlumeUtils.createPollingStream(streamCtx,addresses,StorageLevel.MEMORY_AND_DISK_SER_2,1000,1)
 
-    val flumeStream = FlumeUtils.createPollingStream(streamCtx,address,StorageLevel.MEMORY_AND_DISK_SER_2,1000,1)
     //Define Output Stream Connected to Console for printing the results
     val outputStream = new ObjectOutputStream(Console.out)
     //Invoking custom Print Method for writing Events to Console
-    printValues(flumeStream, streamCtx, outputStream)
+    printValues(flumeStream,streamCtx, outputStream)
 
     //Most important statement which will initiate the Streaming Context
-    streamCtx.start()
+    streamCtx.start();
     //Wait till the execution is completed.
-    streamCtx.awaitTermination()
+    streamCtx.awaitTermination();
   }
 
   def printValues(stream: DStream[SparkFlumeEvent], streamCtx: StreamingContext, outputStream: ObjectOutput): Unit = {
     stream.foreachRDD(foreachFunc)
+    //SparkFlumeEvent is the wrapper classes containing all the events captured by the Stream
     def foreachFunc = (rdd: RDD[SparkFlumeEvent]) => {
       val array = rdd.collect()
-      println("------Start Printing Results------")
-      println("Total size of Events = " + array.size)
-      for (flumeEvent <- array) {
+      println("---------Start Printing Results----------")
+      println("Total size of Events= " +array.size)
+      for(flumeEvent<-array){
         //This is to get the AvroFlumeEvent from SparkFlumeEvent
         //for printing the Original Data
-        val payLoad = flumeEvent // TODO check the bug
-        println(new String(payLoad.toString))
+        val payLoad = flumeEvent.event.getBody()
+        //Printing the actual events captured by the Stream
+        println(new String(payLoad.array()))
       }
       println("---------Finished Printing Results----------")
     }
