000c326426b02fec0d3f63dfe2c7fd3d33f5ddaa
noprom <tyee.noprom@qq.com>
2016-02-23 11:05:10 +0800

    init log generator

---
+++ src/main/java/com/huntdreams/streaming/ch4/LogGenerator.java
@@ -0,0 +1,22 @@
+package com.huntdreams.streaming.ch4;
+
+/**
+ * LogGenerator
+ *
+ * @author tyee.noprom@qq.com
+ * @time 2/23/16 11:02 AM.
+ */
+public class LogGenerator {
+    
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage - java LogGenerator <Location\n" +
+                    " of Log File to be read> <location of the log file in which logs needs\n" +
+                    " to be updated>");
+            System.exit(0);
+        }
+
+
+
+    }
+}
