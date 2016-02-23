7c005c3f109f8753e3b934eecdbc4dab105e52b2
noprom <tyee.noprom@qq.com>
2016-02-23 12:00:10 +0800

    print the values

--- src/main/scala/com/huntdreams/streaming/ch4/ScalaTransformLogEvents.scala
+++ src/main/scala/com/huntdreams/streaming/ch4/ScalaTransformLogEvents.scala
@@ -2,7 +2,8 @@ package com.huntdreams.streaming.ch4
 
 import java.net.InetSocketAddress
 
-import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.flume.FlumeUtils
@@ -49,4 +50,26 @@ object ScalaTransformLogEvents {
    * Define and execute all Transformations to the log data
    */
   def executeTransformations(dStream: DStream[(String, String)], streamCtx: StreamingContext) {}
+
+  /**
+   * Print the values
+   *
+   * @param stream stream
+   * @param streamCtx streamCtx
+   */
+  def printValues(stream: DStream[(String, String)], streamCtx: StreamingContext): Unit = {
+    //Implementing ForEach function for printing all the data in provided DStream
+    stream.foreachRDD(foreachFunc)
+    def foreachFunc = (rdd: RDD[(String, String)]) => {
+      //collect() method fetches the data from all partitions and "collects" at driver node.
+      //So in case data is too huge than driver may crash.
+      //In production environments we persist this RDD data into HDFS or use the rdd.take(n) method.
+      val array = rdd.collect()
+      println("---------Start Printing Results----------")
+      for (dataMap <- array.array) {
+        print(dataMap._1, "-----", dataMap._2)
+      }
+      println("---------Finished Printing Results----------")
+    }
+  }
 }
