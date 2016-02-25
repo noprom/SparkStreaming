3339f3f9cf770bf57d6ee04ac2f789c9cee55efe
noprom <tyee.noprom@qq.com>
2016-02-25 21:29:32 +0800

    query from spark sql

--- src/main/scala/com/huntdreams/streaming/ch6/ScalaFirstSparkSQL.scala
+++ src/main/scala/com/huntdreams/streaming/ch6/ScalaFirstSparkSQL.scala
@@ -32,5 +32,20 @@ object ScalaFirstSparkSQL {
     dataFrame.registerTempTable("company")
     // Printing the Schema of the Data loaded in the Data Frame
     dataFrame.printSchema()
+
+    // Executing SQL Queries to Print all records in the DataFrame
+    println("Printing All records")
+    sqlCtx.sql("Select * from company").collect().foreach(print)
+
+    // Executing SQL Queries to Print Name and Employees
+    // in each Department
+    println("\n Printing Number of Employees in All Departments")
+    sqlCtx.sql("Select Name, No_Of_Emp from company").collect().foreach(println)
+
+    // Using the aggregate function (agg) to print the
+    // total number of employees in the Company
+    println("\n Printing Total Number of Employees in Company_X")
+    val allRec = sqlCtx.sql("Select * from company").agg(Map("No_Of_Emp" -> "sum"))
+    allRec.collect.foreach(println)
   }
 }
