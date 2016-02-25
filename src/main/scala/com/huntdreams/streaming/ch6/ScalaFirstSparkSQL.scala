8d13737bd34afea95ebf0254a7eec153287ccaf9
noprom <tyee.noprom@qq.com>
2016-02-25 20:22:24 +0800

    spark sql in straming

---
+++ src/main/scala/com/huntdreams/streaming/ch6/ScalaFirstSparkSQL.scala
@@ -0,0 +1,36 @@
+package com.huntdreams.streaming.ch6
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * ScalaFirstSparkSQL
+  *
+  * @author tyee.noprom@qq.com
+  * @time 2/25/16 7:45 PM.
+  */
+object ScalaFirstSparkSQL {
+
+  def main(args: Array[String]) {
+    // Creating Spark Configuration
+    val conf = new SparkConf()
+    // Setting Application/Job Name
+    conf.setAppName("My First Spark SQL")
+    // Define Spark Context which we will use to initialize our SQLContext
+    val sparkCtx = new SparkContext(conf)
+    // Creating SQL Context
+    val sqlCtx = new SQLContext(sparkCtx)
+
+    // Define path of your JSON File (company.json) which needs to be processed
+    val path = "/Users/noprom/Documents/Dev/Spark/Data/company.json"
+    // Use SQLCOntext and Load the JSON file.
+    // This will return the DataFrame which can be further Queried using SQL queries.
+    val dataFrame = sqlCtx.jsonFile(path)
+
+    // Register the data as a temporary table within SQL Context
+    // Temporary table is destroyed as soon as SQL Context is destroyed.
+    dataFrame.registerTempTable("company")
+    // Printing the Schema of the Data loaded in the Data Frame
+    dataFrame.printSchema()
+  }
+}
